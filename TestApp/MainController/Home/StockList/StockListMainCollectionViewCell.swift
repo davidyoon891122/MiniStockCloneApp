a1b77c669c72d882fb8008708c43343f8904a286
Yoonjiwon <k112751@koreainvestment.com>
2022-02-15 09:57:13 +0900

    add inset, move sortingButtonHStackView from StockListView to StockListMainCollectionViewCell

--- TestApp/MainController/Home/StockList/StockListMainCollectionViewCell.swift
+++ TestApp/MainController/Home/StockList/StockListMainCollectionViewCell.swift
@@ -11,6 +11,36 @@ class StockListMainCollectionViewCell: UICollectionViewCell {
     static let identifier: String = "StockListMainCollectionViewCell"
     private var menus: [String]?
     private let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple, .black, .white, .systemPink, .gray]
+    private let detailCellHeight: CGFloat = 65.0
+    private lazy var sortingButtonHStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .systemBackground
+        [sortingButton, etfButton]
+            .forEach {
+                stackView.addSubview($0)
+            }
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var sortingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전일 기준 ⌵", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var etfButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("⌾ ETF만 보기", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -69,27 +99,39 @@ extension StockListMainCollectionViewCell: UICollectionViewDataSource {
 
 extension StockListMainCollectionViewCell: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width - 40, height: (732 - 50 - 32) / 10)
+        return CGSize(width: collectionView.frame.width - 40, height: detailCellHeight)
     }
 }
 
 private extension StockListMainCollectionViewCell {
     func addSubviews() {
-        [collectionView, moreButton]
+        [collectionView, moreButton, sortingButtonHStackView]
             .forEach {
                 addSubview($0)
             }
     }
     
     func setLayoutConstraints() {
-        collectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        let inset: CGFloat = 16.0
+        sortingButtonHStackView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        sortingButtonHStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset).isActive = true
+        sortingButtonHStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset).isActive = true
+        sortingButtonHStackView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        sortingButton.centerYAnchor.constraint(equalTo: sortingButtonHStackView.centerYAnchor).isActive = true
+        sortingButton.leadingAnchor.constraint(equalTo: sortingButtonHStackView.leadingAnchor).isActive = true
+        
+        etfButton.centerYAnchor.constraint(equalTo: sortingButtonHStackView.centerYAnchor).isActive = true
+        etfButton.trailingAnchor.constraint(equalTo: sortingButtonHStackView.trailingAnchor).isActive = true
+        
+        collectionView.topAnchor.constraint(equalTo: sortingButtonHStackView.bottomAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         
         moreButton.topAnchor.constraint(equalTo: collectionView.bottomAnchor).isActive = true
-        moreButton.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        moreButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset).isActive = true
         moreButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        moreButton.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        moreButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
+        moreButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset).isActive = true
+        moreButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -inset).isActive = true
     }
 }
--- TestApp/MainController/Home/StockList/StockListView.swift
+++ TestApp/MainController/Home/StockList/StockListView.swift
@@ -32,36 +32,6 @@ class StackListView: UIView {
         return separator
     }()
     
-    private lazy var sortingButtonHStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.backgroundColor = .systemBackground
-        [sortingButton, etfButton]
-            .forEach {
-                stackView.addSubview($0)
-            }
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    private lazy var sortingButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("전일 기준 ⌵", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
-        button.setTitleColor(.lightGray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    private lazy var etfButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("⌾ ETF만 보기", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
-        button.setTitleColor(.lightGray, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -110,7 +80,7 @@ extension StackListView: UICollectionViewDelegateFlowLayout {
 
 private extension StackListView {
     func addSubviews() {
-        [menuCollectionView, separatorView, sortingButtonHStackView, collectionView]
+        [menuCollectionView, separatorView, collectionView]
             .forEach {
                 addSubview($0)
             }
@@ -128,21 +98,10 @@ private extension StackListView {
         separatorView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
         
-        sortingButtonHStackView.topAnchor.constraint(equalTo: separatorView.bottomAnchor).isActive = true
-        sortingButtonHStackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        sortingButtonHStackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        sortingButtonHStackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        sortingButton.leadingAnchor.constraint(equalTo: sortingButtonHStackView.leadingAnchor, constant: 20).isActive = true
-        sortingButton.centerYAnchor.constraint(equalTo: sortingButtonHStackView.centerYAnchor).isActive = true
-        
-        etfButton.trailingAnchor.constraint(equalTo: sortingButtonHStackView.trailingAnchor, constant: -20).isActive = true
-        etfButton.centerYAnchor.constraint(equalTo: sortingButtonHStackView.centerYAnchor).isActive = true
-        
-        collectionView.topAnchor.constraint(equalTo: sortingButtonHStackView.bottomAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: separatorView.bottomAnchor).isActive = true
         collectionView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        collectionView.heightAnchor.constraint(equalToConstant: 700 + 32).isActive = true
+        collectionView.heightAnchor.constraint(equalToConstant: 730 + 16).isActive = true
     }
 }
