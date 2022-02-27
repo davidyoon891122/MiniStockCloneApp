74ff440613336d87a0f2e7265a800c179f110bc2
Yoonjiwon <k112751@koreainvestment.com>
2022-02-28 08:41:51 +0900

    apply snapKit on HomeVC

--- TestApp/Delegates/SceneDelegate.swift
+++ TestApp/Delegates/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    let switchMenu = SwitchMenu.test
+    let switchMenu = SwitchMenu.real
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
