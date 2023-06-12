3b14374a42b920c2adca5f880c85cd44812542c7
Davidyoon <davidyoon@koreainvestment.com>
2023-06-12 10:19:17 +0900

    Update the URLInfo
    * Updated the urlInfo
    * Updated the KingFisher lib

--- TestApp/Utils/URLInfo.swift
+++ TestApp/Utils/URLInfo.swift
@@ -16,26 +16,26 @@ enum URLInfo {
     var url: URL? {
         switch self {
         case .stock:
-            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/my-stock")
+            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/mini/my-stock")
         case .dividend:
-            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/dividend-list")
+            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/mini/dividend-list")
         case .profit:
-            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/my-profit")
+            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/mini/my-profit")
         case .increase:
-            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/increase-list")
+            return URL(string: "https://boiling-scrubland-57180.herokuapp.com/mini/increase-list")
         }
     }
 
     var localURL: URL? {
         switch self {
         case .stock:
-            return URL(string: "http://127.0.0.1:3000/my-stock")
+            return URL(string: "http://127.0.0.1:3000/mini/my-stock")
         case .dividend:
-            return URL(string: "http://127.0.0.1:3000/dividend-list")
+            return URL(string: "http://127.0.0.1:3000/mini/dividend-list")
         case .profit:
-            return URL(string: "http://127.0.0.1:3000/my-profit")
+            return URL(string: "http://127.0.0.1:3000/mini/my-profit")
         case .increase:
-            return URL(string: "http://127.0.0.1:3000/increase-list")
+            return URL(string: "http://127.0.0.1:3000/mini/increase-list")
         }
     }
 }
